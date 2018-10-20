231d339bd3b928e62bd3c58bc2ac1dd507761af1
MininMobile <notminin@pm.me>
2018-10-20 16:01:35 +0100

    add init

---
+++ src/vector-map.js
@@ -0,0 +1,31 @@
+let points = [
+	{ x: 100, y: 100 },
+	{ x: 245, y: 214 },
+	{ x: 312, y: 412 },
+	{ x: 432, y: 123 },
+	{ x: 166, y: 122 },
+	{ x: 85, y: 232 },
+	{ x: 219, y: 271 },
+	{ x: 379, y: 370 },
+	{ x: 377, y: 199 },
+	{ x: 277, y: 122 },
+	{ x: 322, y: 285 },
+	{ x: 164, y: 355 }
+];
+
+function setup() {
+	createCanvas(800, 600);
+
+	fill(255);
+	noStroke(0);
+}
+
+function draw() {
+	background("#1c1c1c");
+
+	// map
+	drawPoints();
+
+	// cursor
+	ellipse(mouseX, mouseY, 20);
+}
