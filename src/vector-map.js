6e50c8695cb44acd673db4b5b16b9d1abc0a2f7f
MininMobile <notminin@pm.me>
2018-10-20 16:22:53 +0100

    draw lines

--- src/vector-map.js
+++ src/vector-map.js
@@ -1,16 +1,9 @@
 let points = [
+	{ x: 0, y: 0 },
 	{ x: 100, y: 100 },
 	{ x: 245, y: 214 },
 	{ x: 312, y: 412 },
-	{ x: 432, y: 123 },
-	{ x: 166, y: 122 },
-	{ x: 85, y: 232 },
-	{ x: 219, y: 271 },
-	{ x: 379, y: 370 },
-	{ x: 377, y: 199 },
-	{ x: 277, y: 122 },
-	{ x: 322, y: 285 },
-	{ x: 164, y: 355 }
+	{ x: 432, y: 123 }
 ];
 
 function setup() {
@@ -23,28 +16,48 @@ function setup() {
 function draw() {
 	background("#1c1c1c");
 
+	// cursor
+	points[0].x = mouseX;
+	points[0].y = mouseY;
+
 	// map
 	drawPoints();
-
-	// cursor
-	ellipse(mouseX, mouseY, 20);
+	drawLines();
 }
 
 function mouseClicked() {
 	if (mouseButton == LEFT) {
 		points.push({
-			x: mouseX,
-			y: mouseY
+			x: points[0].x,
+			y: points[0].y
 		});
 	}
 }
 
 //
-// util
+// util: draw
 //
 
 function drawPoints() {
 	points.forEach((point) => {
 		ellipse(point.x, point.y, 20);
 	});
+}
+
+function drawLines() {
+	stroke(255);
+	strokeWeight(2);
+
+	for (let i = 0; i < points.length; i++) {
+		let point1 = points[i];
+		for (let j = 0; j < points.length; j++) {
+			let point2 = points[j];
+
+			if (point1 == point2) continue;
+
+			line(point1.x, point1.y, point2.x, point2.y);
+		}
+	}
+
+	noStroke();
 }
