5b9738dea3ccd27637b61d1be63960fbed9fe9e8
not ervin <notminin@pm.me>
2019-06-02 16:00:09 +0100

    draw grid and connect dots

--- src/vector-map.js
+++ src/vector-map.js
@@ -1,8 +1,10 @@
-let points = [
-	{ x: 0, y: 0 },
-]
+let objects = new Map();
+
+let grid = { enabled: true, size: 50 }
 
 let mouse = { hover: false, x: 0, y: 0 }
+
+let selected = undefined;
 
 // create canvas
 let cp = document.getElementById("placeholder");
@@ -27,7 +29,7 @@ addEventListener("load", () => {
 // create context
 let ctx = c.getContext("2d");
 ctx.lineWidth = 2;
-ctx.font = "1em Arial";
+ctx.font = "1em sans-serif";
 
 (function render() {
 	requestAnimationFrame(render);
@@ -35,39 +37,82 @@ ctx.font = "1em Arial";
 	ctx.fillStyle = "#1c1c1c";
 	ctx.fillRect(0, 0, cp.clientWidth, cp.clientHeight);
 
-	// cursor
-	points[0].x = mouse.x;
-	points[0].y = mouse.y;
-
-	// map
-	for (let i = mouse.hover ? 0 : 1; i < points.length; i++) {
-		let point1 = points[i];
-		
-		for (let j = mouse.hover ? 0 : 1; j < points.length; j++) {
-			let point2 = points[j];
-
-			if (point1 == point2) continue;
-
-			let d = distanceTo(point1, point2);
-
-			if (255 - d <= 0) continue;
-
-			ctx.strokeStyle = `rgba(255, 255, 255, ${(255 - d) / 255})`;
+	// grid
+	if (grid.enabled) {
+		{ // background
+			ctx.strokeStyle = "#333"
 			ctx.beginPath();
-			ctx.moveTo(point1.x, point1.y);
-			ctx.lineTo(point2.x, point2.y);
+
+			for (let i = cp.clientWidth / 2; i > 0; i -= grid.size) {
+				ctx.moveTo(i, 0);
+				ctx.lineTo(i, cp.clientHeight);
+			}
+
+			for (let i = cp.clientWidth / 2; i < cp.clientWidth; i += grid.size) {
+				ctx.moveTo(i, 0);
+				ctx.lineTo(i, cp.clientHeight);
+			}
+
+			for (let i = cp.clientHeight / 2; i > 0; i -= grid.size) {
+				ctx.moveTo(0, i);
+				ctx.lineTo(cp.clientWidth, i);
+			}
+
+			for (let i = cp.clientHeight / 2; i < cp.clientHeight; i += grid.size) {
+				ctx.moveTo(0, i);
+				ctx.lineTo(cp.clientWidth, i);
+			}
+
+			ctx.stroke();
+		}
+
+		{ // middle
+			ctx.strokeStyle = "#555"
+			ctx.beginPath();
+
+			ctx.moveTo(cp.clientWidth / 2, 0);
+			ctx.lineTo(cp.clientWidth / 2, cp.clientHeight);
+
+			ctx.moveTo(0, cp.clientHeight / 2);
+			ctx.lineTo(cp.clientWidth, cp.clientHeight / 2);
+
 			ctx.stroke();
 		}
 	}
 
-	ctx.fillStyle = "#fff";
-	for (let i = mouse.hover ? 0 : 1; i < points.length; i++) {
-		let point = points[i];
+	// map
+	objects.forEach((points) => {
+		for (let i = 0; i < points.length; i++) {
+			let point1 = points[i];
 
-		ctx.beginPath();
-		ctx.ellipse(point.x, point.y, 10, 10, 0, 0, Math.PI * 2);
-		ctx.fill();
-	}
+			for (let j = 0; j < points.length; j++) {
+				let point2 = points[j];
+
+				if (point1 == point2) continue;
+
+				let d = distanceTo(point1, point2);
+
+				if (255 - d <= 0) continue;
+
+				ctx.strokeStyle = `rgba(255, 255, 255, ${(255 - d) / 255})`;
+				ctx.beginPath();
+				ctx.moveTo(point1.x, point1.y);
+				ctx.lineTo(point2.x, point2.y);
+				ctx.stroke();
+			}
+		}
+	});
+
+	ctx.fillStyle = "#fff";
+	objects.forEach((points) => {
+		for (let i = 0; i < points.length; i++) {
+			let point = points[i];
+
+			ctx.beginPath();
+			ctx.ellipse(point.x, point.y, 10, 10, 0, 0, Math.PI * 2);
+			ctx.fill();
+		}
+	});
 })();
 
 // mouse controls
